166d9c79915bf007fd108b811f27a73e1eff6dc6
Holger Neub <holger.neub@haufe-lexware.com>
2022-04-24 09:33:35 +0200

    removing unused config values

--- app/config.py
+++ app/config.py
@@ -4,7 +4,6 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     RIPPLED_SERVER_URL: str
     COINSTAT_API_URL : str
-    ACCOUNT : str
     CURRENCY : str = 'EUR'
     API_USER_NAME : str
     API_USER_PASSWORD : str
